9b91b308b4189cbd62e48413e5aa766ac7d21b33
Celine Valentine <cyv419@gmail.com>
2020-09-01 14:15:47 -0700

    test errors

--- seed.py
+++ seed.py
@@ -9,15 +9,21 @@ db.create_all()
 
 # If table isn't empty, empty it
 User.query.delete()
+Post.query.delete()
+Tag.query.delete()
+PostTag.query.delete()
 
 # Add users
 Dave_Allen = User(first_name='Dave', last_name="Allen", image_url='https://www.google.com/imgres?imgurl=https%3A%2F%2Fsrkheadshotday.com%2Fwp-content%2Fuploads%2FKen_Nguyen_Headshot_16H3626.jpg&imgrefurl=https%3A%2F%2Fsrkheadshotday.com%2Fblog%2Fthe-best-tie-knot-for-your-headshot%2F&tbnid=f0mePlVDhAIxNM&vet=12ahUKEwihs4-86o_rAhVV454KHSFmCCIQMygOegUIARDuAQ..i&docid=UobcMAJU5BRp9M&w=1710&h=1140&q=head%20shot&ved=2ahUKEwihs4-86o_rAhVV454KHSFmCCIQMygOegUIARDuAQ')
 Dana_Pine = User(first_name='Dana', last_name="Pine", image_url='https://a57.foxnews.com/hp.foxnews.com/images/2020/08/320/180/5ab97ed145d3b286b7ff65f251387ede.jpg')
 Mark_Thompson = User(first_name='Mark', last_name="Thompson", image_url='https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe3%2F7e%2F0e%2Fe37e0e25686c2139b281a57a5b4906f2.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F460704236854562822%2F&tbnid=GBQUgulcaGKMqM&vet=12ahUKEwihs4-86o_rAhVV454KHSFmCCIQMygDegUIARDYAQ..i&docid=ggzHqVML9AbjqM&w=3744&h=5616&q=head%20shot&ved=2ahUKEwihs4-86o_rAhVV454KHSFmCCIQMygDegUIARDYAQ')
 
+#add posts
+
 post1 = Post(title='teacher', content="abcdefg", created_at="2020.01.01", user_id=2)
 post2 = Post(title='king', content="uxmn", created_at="2020.01.02", user_id=2)
 
+#add tags
 tag1 = Tag(name="teacher")
 tag2 = Tag(name="king")
 
--- test_app.py
+++ test_app.py
@@ -2,12 +2,14 @@ from unittest import TestCase
 
 from app import app
 from models import db, User, Post
-import seed
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEGUB_TB_HOSTS'] = ['dont-show-debug-toolbar']
 app.config['TESTING'] = True
 
+db.drop_all()
+db.create_all()
 
 
 class AppTestCase(TestCase):
@@ -15,41 +17,34 @@ class AppTestCase(TestCase):
 
     def setUp(self):
         """Add sample pet."""
+        User.query.delete()
+        Post.query.delete()
 
-        # user = User(first_name="John_t", last_name="Doe_t")
-        # db.session.add(user)
-        # db.session.commit()
+        user = User(first_name="John", last_name="Doe", image_url="https://www.google.com/imgres?imgurl=https%3A%2F%2Fsrkheadshotday.com%2Fwp-content%2Fuploads%2FKen_Nguyen_Headshot_16H3626.jpg&imgrefurl=https%3A%2F%2Fsrkheadshotday.com%2Fblog%2Fthe-best-tie-knot-for-your-headshot%2F&tbnid=f0mePlVDhAIxNM&vet=12ahUKEwihs4-86o_rAhVV454KHSFmCCIQMygOegUIARDuAQ..i&docid=UobcMAJU5BRp9M&w=1710&h=1140&q=head%20shot&ved=2ahUKEwihs4-86o_rAhVV454KHSFmCCIQMygOegUIARDuAQ")
+        db.session.add(user)
+        db.session.commit()
 
-        # self.user_id = user.id
-        # self.user = user
+        self.user_id = user.id
+        self.user = user
 
-        # Post.query.delete()
+        post = Post(title="teacher", content="abc", user_id="1")
+        db.session.add(post)
+        db.session.commit()
 
-        # post = Post(title="teacher_t", content="abc_t", user_id="1")
-        # db.session.add(post)
-        # db.session.commit()
-
-        # self.post = post
-        # self.post_id = post.id
+        self.post = post
+        self.post_id = post.id
 
     def tearDown(self):
         """Clean up any fouled transaction."""
 
         db.session.rollback()
     
-    def test_root(self):
+    def test_homepage(self):
         with app.test_client() as client:
             response = client.get('/')
             self.assertEqual(response.status_code, 200)
             html = response.get_data(as_text=True)
             self.AssertIn("<h1>Blogly Recent Posts</h1>", html)
-    
-    def test_datetime_filter(self):
-        with app.test_client()as client:
-            response = client.get('/')
-            self.assertEqual(response.status_code, 200)
-            html = response.get_data(as_text=True)
-            regex = 
 
     def test_list_users(self):
         with app.test_client() as client:
@@ -57,11 +52,11 @@ class AppTestCase(TestCase):
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
-            self.assertIn('Allen, Dave', html)
+            self.assertIn('Doe, John', html)
 
     def test_user_detail(self):
         with app.test_client() as client:
-            resp = client.get(f"/users/1")
+            resp = client.get(f"/users/{self.user_id}")
             html = resp.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
@@ -70,11 +65,20 @@ class AppTestCase(TestCase):
     def test_add_user(self):
         with app.test_client() as client:
             d = {"first_name": "James", "last_name": "Smith"}
-            resp = client.post("/users/new", data=d, follow_redirects=True)
-            html = resp.get_data(as_text=True)
+            response = client.post("/users/new", data=d, follow_redirects=True)
+            html = response.get_data(as_text=True)
 
             self.assertEqual(resp.status_code, 200)
             self.assertIn("<h1>James Smith</h1>", html)
+    def test_edit_user(self):
+        with app.test_client() as client:
+            d = {first_name: Jane, last_name: Doe}
+            response = client.post(f'/users/{self.user_id}/edit', data = d,follow_redirects=True) 
+            html = response.get_data(as_text=True)
+            
+            user = User.query.get(self.user_id)
+            self.assertEqual("Jane", html)
+            
     
     def test_delete_user(self):
         with app.test_client() as client:
@@ -85,7 +89,7 @@ class AppTestCase(TestCase):
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Allen,Dave",html)
 
-            response = client.get("/users/1/delete", follow_redirects=True)
+            response = client.get(f"/users/{self.user_id}/delete", follow_redirects=True)
             self.assertEqual(response.status_code, 200)
             response = client.get('/users')
             html = response.get_data(as_text=True)
@@ -127,5 +131,5 @@ def test_tag_new(self):
         html = resp.get_data(as_text=True)
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn("<a>fun</a>", html)
+        self.assertIn("fun", html)
         
